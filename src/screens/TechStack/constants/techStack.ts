2fe04d9d03af4c99086daf0c7122e087c6e237ad
Sergey Guchok <gck.sergey@gmail.com>
2025-02-10 02:36:50 +0300

    change tech descriptions

--- src/screens/TechStack/constants/techStack.ts
+++ src/screens/TechStack/constants/techStack.ts
@@ -1,47 +1,47 @@
 export const TECH_STACK_PRIMARY = [
   {
     name: "Next",
-    description: "Full-Stack React Framework",
+    description: "React framework for production-grade applications",
     icon: "next",
   },
   {
     name: "React",
-    description: "Full-Stack React Framework",
+    description: "Library for building user interfaces",
     icon: "react",
   },
   {
     name: "Tailwind CSS",
-    description: "Full-Stack React Framework",
+    description: "Utility-first CSS framework",
     icon: "tailwind",
   },
   {
     name: "Supabase",
-    description: "Full-Stack React Framework",
+    description: "Open source Firebase alternative",
     icon: "supabase",
   },
   {
     name: "TypeScript",
-    description: "Full-Stack React Framework",
+    description: "JavaScript with syntax for types",
     icon: "typescript",
   },
   {
     name: "Tanstack",
-    description: "Full-Stack React Framework",
+    description: "Powerful data management libraries",
     icon: "tanstack",
   },
   {
     name: "Docker",
-    description: "Full-Stack React Framework",
+    description: "Platform for containerized applications",
     icon: "docker",
   },
   {
     name: "Sequelize",
-    description: "Full-Stack React Framework",
+    description: "Modern TypeScript ORM for Node.js",
     icon: "sequelize",
   },
   {
     name: "Mongo",
-    description: "Full-Stack React Framework",
+    description: "NoSQL document database",
     icon: "mongo",
   },
 ];
